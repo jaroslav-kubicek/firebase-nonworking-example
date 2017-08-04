35c6f4e9d4c609793047fa38dae814b825e6ad06
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2017-08-04 23:37:26 +0200

    fix spaces

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 import * as firebase from 'firebase';
 
 const onClick = () => {
-    const config = {
-      apiKey: '',
-      authDomain: '',
-      databaseURL: '',
-      projectId: '',
-      storageBucket: '',
-      messagingSenderId: ''
-    };
-	firebase.initializeApp(config);
-	firebase.auth().signInAnonymously()
-	  .then(() => {
-		  console.log(firebase.auth().currentUser.uid);
-	  })
-	  .catch((error) => console.log(error));	
+  const config = {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+  };
+  firebase.initializeApp(config);
+  firebase.auth().signInAnonymously()
+    .then(() => {
+      console.log(firebase.auth().currentUser.uid);
+    })
+    .catch((error) => console.log(error));	
 };
 
 document.querySelector('#firebase').addEventListener('click', onClick, false);
